5682e74e8f7211267eb9c386417d001d7dbdb6ab
test <test@test.com>
2021-06-24 20:37:38 +0800

    6-24

--- 94 Crypto multilayer/solve.py
+++ 94 Crypto multilayer/solve.py
@@ -1,38 +1,55 @@
-import base64
 import re
 import json
+import base64
 import binascii
-import collections
 from gmpy2 import *
-
 from Crypto.Util import number
 
-pattern1=re.compile('RSA\(n=(.*?), e=(.*?)\)')
-pattern2=re.compile('Counter\((.*?)\)')
 
+
+patt1_rsa=re.compile("RSA\(n=(.*), e=(.*)\)")
+patt2_dic=re.compile('Counter\((.*?)\)')
+
+f=open("encrypted","r")
+line1=f.readline()
+line2=f.readline()
+line3=f.readline()
+line4=f.readline()
+
+n,e=patt1_rsa.findall(line1)[0]
+n=int(n,16)
+e=int(e,16)
+
+
+
+
+def xor(a, b):
+    return bytes(i ^ j for i, j in zip(a, b))
 def rsa_encrypt(x):
     v = number.bytes_to_long(x)
     return pow(v, e, n)
-    
-def xor(a, b):
-    return bytes(i ^ j for i, j in zip(a, b))
-    
-    
-    
-def layer4_dec2(data,lenth):
+
+
+
+def compute_length(data):
+    raw=patt2_dic.findall(data)[0].replace('\'','\"')
+    counter=json.loads(raw)
+    length=0
+    for char,num in counter.items():
+        length=length+num
+    return length
+def layer4_dec(data):
+    xx={}
+    for i in range(0x10000):
+        r = rsa_encrypt(('%.4x' % i).encode('latin1'))
+        block = binascii.unhexlify('%.512x' % r)
+        xx[block]=('%.4x' % i)
     data=base64.b64decode(data)
-    data=xor(data[:-256],data[256:])
-    output=b''
-    for i in range(0, len(data), 256):
-        r=int(binascii.hexlify(data[i:i+256]),16)
-        for j in range(0x10000):
-            t=rsa_encrypt(('%.4x' % j).encode('latin1'))
-            if r==t:
-                output=output+binascii.unhexlify('%.4x' % j)
-                print(output)
-                break
-    return output[:lenth]
-    
+    blocks=xor(data[:-256],data[256:])
+    padded=""
+    for i in range(0,len(blocks),256):
+        padded=padded+xx[blocks[i:i+256]]
+    return binascii.unhexlify(padded)
 def layer2_3_dec(data):
     d=invert(17,251)
     for key in range(0x100):
@@ -44,34 +61,7 @@ def layer2_3_dec(data):
         if output[4:5]==b'{' and output[-2:]==b'}\n':
             return output
 
-f=open('encrypted','r')
-rsa=f.readline().strip('\n')
-sha256=f.readline().strip('\n')
-counter=f.readline().strip('\n')
-enc_data=f.readline().strip('\n')
-f.close()
-
-n,e=pattern1.findall(rsa)[0]
-n=int(n,16)
-e=int(e,16)
-
-
-counter=pattern2.findall(counter)[0]
-counter=json.loads(counter.replace('\'','\"'))
-
-
-length=0
-for char,num in counter.items():
-    length=length+num
-
-
-flag=layer4_dec2(enc_data,length)
-print(flag)
-flag = layer2_3_dec(flag)
-print(flag)
-
-
-
-
-
-
+length=compute_length(line3)
+data=layer4_dec(line4)[:length]
+data=layer2_3_dec(data)
+print(data)
